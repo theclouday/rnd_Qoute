824255f48d5f2c96d476d83325dca40b6c8cbac6
theclouday <theclouday@example.com>
2024-02-07 20:12:08 +0100

    add event for click on button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 "use strict";
 
 const someWords = ['пока не упал духом любые другие падения по плечу eсли до вашей планки не дотягиваются это не повод ее занижать нельзя упускать возможность чему-то научиться мечты так и остаются мечтами если к ним не идти человек всесилен пока ничем не занят победившему себя ничего не страшно обращаясь к прошлому мы поворачиваемся спиной к будущему эмоции возникают от мыслей не нравится внутреннее состояние нужно менять мышление смысл жизни есть всегда его нужно только найти мстить это то же самое что кусать в ответ собаку только тот по-настоящему счастлив и велик кому не нужно ни подчиняться ни приказывать для того чтобы представлять собой что-то'];
+const butMain = document.querySelector('.buttonCreate');
+
+
 
 function makeQuote() {
     let out ='';
-    const quoteLenght = Math.floor(Math.random() * 12 + 1);
+    const quoteLenght = Math.floor(Math.random() * 12 + 1); // Здесь я строго задаю длинну цитаты
     console.log(quoteLenght);
     const quoteOutput = document.getElementById("quoteOutput");
     let quoteOutputE = '';
@@ -18,4 +21,6 @@ function makeQuote() {
 
 }
 
-makeQuote();    
+
+butMain.addEventListener('click', makeQuote)
+// makeQuote();    
